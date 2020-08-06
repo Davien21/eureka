8ba9cbc8f891a809d192f5967394fabc96fdd1eb
davien21 <chidiebereekennia@gmail.com>
2020-08-06 04:19:48 +0100

    test clear db 4

--- backends/classes/Login.php
+++ backends/classes/Login.php
@@ -19,28 +19,7 @@ CREATE TABLE `heroku_a90165f761347d5`.`staff_list` (
 	status VARCHAR (20) DEFAULT 'enabled',
 	date_added timestamp,
 	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE `heroku_a90165f761347d5`.`hospital_list` (
-	id INT AUTO_INCREMENT PRIMARY KEY, 
-	unique_id VARCHAR(11),
-	name VARCHAR(255),
-	director_id INT(11),
-	status VARCHAR (20) DEFAULT 'enabled',
-	date_added timestamp,
-	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE `heroku_a90165f761347d5`.`director_list` (
-	id INT AUTO_INCREMENT PRIMARY KEY, 
-	hospital_id VARCHAR(255),
-	f_name VARCHAR(255),
-	l_name VARCHAR(255),
-	email VARCHAR(250) UNIQUE KEY,
-	phone VARCHAR(14),
-	status VARCHAR (20) DEFAULT 'enabled',
-	date_added timestamp,
-	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-";
+)";
 			$check_query = PDO::prepare($sql);
 			$check_query->execute([':input'=>$identity]);
 			print_r($check_query->errorInfo());
