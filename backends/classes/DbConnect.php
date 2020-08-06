f55029f811a7b54ec8f4784def16e50f072c6758
davien21 <chidiebereekennia@gmail.com>
2020-08-06 03:04:30 +0100

    test clear db 3

--- backends/classes/DbConnect.php
+++ backends/classes/DbConnect.php
@@ -1,6 +1,13 @@
 <?php
 	// echo "Db connect linked!";
 	class DbConnect extends PDO {
+		// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+		// $server = $url["host"];
+		// $username = $url["user"];
+		// $password = $url["pass"];
+		// $db_name = substr($url["path"], 1);
+
 		private $host = "localhost";
 		private $db_name = 'eureka';
 		private $user_name = "root";
--- backends/login-b.php
+++ backends/login-b.php
@@ -1,66 +1,60 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	
+	require 'backends/classes/DbConnect.php';
+	// require 'database/classes/SelectQuery.php';
+	// require 'database/classes/UpdateQuery.php';
+	require 'backends/classes/FormValidator.php';
+	require 'backends/classes/Registration.php';
+	require 'backends/classes/Login.php';
+	session_start();
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-print_r($conn);
-	// require 'backends/classes/DbConnect.php';
-	// // require 'database/classes/SelectQuery.php';
-	// // require 'database/classes/UpdateQuery.php';
-	// require 'backends/classes/FormValidator.php';
-	// require 'backends/classes/Registration.php';
-	// require 'backends/classes/Login.php';
-	// session_start();
-
-	// // echo $form_validator->alpha_num_rand(10);
-	// // $login->director_login();
-	// if (strstr($_GET['t'], 'director')) $login_type = 'director';
-	// else if (strstr($_GET['t'], 'staff')) $login_type = 'staff';
-
-	// if (isset($_POST['login'])) {
-	// 	$login = new Login();
-	// 	$form_val = new FormValidator();
-	// 	// echo "string";
-	// 	// //Declarations
+	// echo $form_validator->alpha_num_rand(10);
+	// $login->director_login();
+	if (strstr($_GET['t'], 'director')) $login_type = 'director';
+	else if (strstr($_GET['t'], 'staff')) $login_type = 'staff';
+	foreach ($_SERVER as $key => $value) {
+		echo "{$key} -> {$value}<br>";
+	}
+	if (isset($_POST['login'])) {
+		$login = new Login();
+		$form_val = new FormValidator();
+		// echo "string";
+		// //Declarations
 
 		
-	// 	$identity  = $form_val->sanitizeInputs(strtolower($_POST['identity']));
-	// 	$pass  = $form_val->sanitizePasswords($_POST['pass']);
+		$identity  = $form_val->sanitizeInputs(strtolower($_POST['identity']));
+		$pass  = $form_val->sanitizePasswords($_POST['pass']);
 		
-	// 	// //Error handling
-	// 	$identity_err = $form_val->check_if_empty($identity, 'Email or Phone Number')['error'];
-	// 	$pass_err = $form_val->check_if_empty($pass, 'Password')['error'];
-	// 	$all_errors = [$identity_err,$pass_err];
-	// 	if (!$form_val->check_all_errors($all_errors)) {
-	// 		// echo $form_val->hashPassword($pass);
-	// 		$is_valid_login  = $login->check_valid_login($login_type,$identity,$pass);
-	// 		if ($is_valid_login) {
-	// 			$identity_err = $form_val->unregistered_user();
-	// 		}else {
+		// //Error handling
+		$identity_err = $form_val->check_if_empty($identity, 'Email or Phone Number')['error'];
+		$pass_err = $form_val->check_if_empty($pass, 'Password')['error'];
+		$all_errors = [$identity_err,$pass_err];
+		if (!$form_val->check_all_errors($all_errors)) {
+			// echo $form_val->hashPassword($pass);
+			$is_valid_login  = $login->check_valid_login($login_type,$identity,$pass);
+			if ($is_valid_login) {
+				$identity_err = $form_val->unregistered_user();
+			}else {
 
-	// 		}
+			}
 			
-	// 		// $is_valid_admin = grant_login_access ($identity,'admin_list','worldof_wow');
-	// 		// if (!empty($is_valid_admin)) {
-	// 		// 	$correct_pass = $select_query->check_if_passwords_match ($identity,$pass,'admin_list','worldof_wow');
-	// 		// 	if (!$correct_pass) {
-	// 		// 		$pass_err = 'Wrong Password';
-	// 		// 	}else {
-	// 		// 		$_SESSION['user'] = 'admin';
-	// 		// 		$_SESSION['id'] = $is_valid_admin['id'];
-	// 		// 		header('Location:admin/home.php');
-	// 		// 	}
-	// 		// }else {
-	// 		// 	$identity_err = 'Invalid identity Address';
-	// 		// }
-	// 	}
-	// }else {
+			// $is_valid_admin = grant_login_access ($identity,'admin_list','worldof_wow');
+			// if (!empty($is_valid_admin)) {
+			// 	$correct_pass = $select_query->check_if_passwords_match ($identity,$pass,'admin_list','worldof_wow');
+			// 	if (!$correct_pass) {
+			// 		$pass_err = 'Wrong Password';
+			// 	}else {
+			// 		$_SESSION['user'] = 'admin';
+			// 		$_SESSION['id'] = $is_valid_admin['id'];
+			// 		header('Location:admin/home.php');
+			// 	}
+			// }else {
+			// 	$identity_err = 'Invalid identity Address';
+			// }
+		}
+	}else {
 		$identity  = '';
 		$identity_err = '';
 		$pass_err = '';
-	// }
+	}
 ?>
