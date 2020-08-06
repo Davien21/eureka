f87b4b4404ff6a28df9d1fe0157af9a7bcfb2080
davien21 <chidiebereekennia@gmail.com>
2020-08-06 04:18:25 +0100

    test clear db 4

--- backends/classes/Login.php
+++ backends/classes/Login.php
@@ -17,7 +17,7 @@ CREATE TABLE `heroku_a90165f761347d5`.`staff_list` (
 	phone VARCHAR(14),
 	job_title VARCHAR(255),
 	status VARCHAR (20) DEFAULT 'enabled',
-	date_added timestamp DEFAULT,
+	date_added timestamp,
 	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `heroku_a90165f761347d5`.`hospital_list` (
@@ -26,7 +26,7 @@ CREATE TABLE `heroku_a90165f761347d5`.`hospital_list` (
 	name VARCHAR(255),
 	director_id INT(11),
 	status VARCHAR (20) DEFAULT 'enabled',
-	date_added timestamp DEFAULT,
+	date_added timestamp,
 	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `heroku_a90165f761347d5`.`director_list` (
@@ -37,7 +37,7 @@ CREATE TABLE `heroku_a90165f761347d5`.`director_list` (
 	email VARCHAR(250) UNIQUE KEY,
 	phone VARCHAR(14),
 	status VARCHAR (20) DEFAULT 'enabled',
-	date_added timestamp DEFAULT,
+	date_added timestamp,
 	last_edited timestamp DEFAULT ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 ";
