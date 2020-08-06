a8a12ef7e98c3c0d5599e142ccc1f1b273f41839
davien21 <chidiebereekennia@gmail.com>
2020-08-06 03:22:43 +0100

    test clear db 4

--- backends/classes/DbConnect.php
+++ backends/classes/DbConnect.php
@@ -1,12 +1,19 @@
 <?php
 	// echo "Db connect linked!";
 	class DbConnect extends PDO {
-		// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-		// $server = $url["host"];
-		// $username = $url["user"];
-		// $password = $url["pass"];
-		// $db_name = substr($url["path"], 1);
+		if ($_SERVER['HTTP_HOST']==='eureka-hms.herokuapp.com') {
+			private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+			// $conn_type = 'heroku';
+			private $heroku_host = $this->$url["host"];
+			private $heroku_user_name = $this->$url["user"];
+			private $heroku_password = $this->$url["pass"];
+			private $heroku_db_name = substr($this->$url["path"], 1);
+		}else {
+			// $conn_type = 'local';
+		}
+
+		// $conn = new mysqli($server, $username, $password, $db);
 
 		private $host = "localhost";
 		private $db_name = 'eureka';
@@ -18,10 +25,14 @@
 		}
 		public function __construct () {
 			try {
-				$dsn = "mysql:host=".$this->host.";dbname=".$this->db_name;
+				if ($_SERVER['HTTP_HOST']==='eureka-hms.herokuapp.com') {
+					$dsn = "mysql:host=".$this->heroku_host.";dbname=".$this->heroku_db_name;
+					echo "Connected to heroku Database successfully"."<br>";
+				}else {
+					$dsn = "mysql:host=".$this->host.";dbname=".$this->db_name;
+					echo "Connected to local Database successfully"."<br>";
+				}
 				parent::__construct($dsn,$this->user_name,$this->password);
-			
-				// echo "Connected to Database successfully"."<br>";
 			}catch (Exception $e) {
 				echo "Error - ".$e->getMessage();
 			}
