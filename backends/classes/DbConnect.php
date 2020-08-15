37175a775ba6566dbbae5a6cd9b371dbbee25861
davien21 <chidiebereekennia@gmail.com>
2020-08-15 14:33:16 +0100

    added local db connection and remote db connection functions

--- backends/classes/DbConnect.php
+++ backends/classes/DbConnect.php
@@ -5,32 +5,29 @@
 *change the details if you wish to change to localhost
 */
 	class DbConnect extends PDO {
-		private $host = "remotemysql.com";
-		private $db_name = '3vOS7K3O7v';
-		private $user_name = "3vOS7K3O7v";
-		private $password = "hKSP0tCy8V";
-		private $dsn;
-		private function heroku_db_conn () {
-			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-			$server = $url["host"];
-			$username = $url["user"];
-			$password = $url["pass"];
-			$db = substr($url["path"], 1);
-			$dsn = "mysql:host=".$server.";dbname=".$db;
-
-			parent::__construct($dsn,$username,$password);
-
-			// $conn = new mysqli($server, $username, $password, $db);
-			
+		private function local_conn() {
+			$host = "localhost";
+			$user = "root";
+			$pass = "";
+			$dsn = "mysql:host=".$host.";dbname=eureka";
+			return $conn = ['dsn'=>$dsn,'user'=>$user,'pass'=>$pass];
 		}
-		public function getDbname () {
-			return $this->db_name;
+		private function remote_db_conn () {
+			$host = "remotemysql.com";
+			$db = '3vOS7K3O7v';
+			$user = "3vOS7K3O7v";
+			$pass = "hKSP0tCy8V";
+			$dsn = "mysql:host=".$server.";dbname=".$db;
+			return $conn = ['dsn'=>$dsn,'user'=>$user,'pass'=>$pass];
 		}
 		public function __construct () {
 			try {
-				$dsn = "mysql:host=".$this->host.";dbname=".$this->db_name;
-				// echo "Connected to remote Database successfully"."<br>";
-				parent::__construct($dsn,$this->user_name,$this->password);
+				if ($_SERVER['HTTP_HOST'] !== 'localhost') {
+					$conn = $this->remote_db_conn();
+				}else {
+					$conn = $this->local_conn();
+				}
+				parent::__construct($conn['dsn'],$conn['user'],$conn['pass']);
 			}catch (Exception $e) {
 				echo "Error - ".$e->getMessage();
 			}
