497b415cfb0164b5a1826b6df6fc1a43394dda3b
davien21 <chidiebereekennia@gmail.com>
2020-08-22 18:57:58 +0100

    testing __DIR__ bug -6

--- user/backends/home-b.php
+++ user/backends/home-b.php
@@ -1,13 +1,13 @@
 <?php
 	session_start();
-	if (!isset($_SESSION['user'])) return  header('Location:../login.php');
-	include '../backends/classes/DBConnect.php';
-	include './backends/classes/UserProfile.php';
-	include '../backends/classes/HospitalSetup.php';
-	include '../backends/classes/FormValidator.php';
-	$id = $_SESSION['user'];
-	$user_profile = new UserProfile($id);
-	$user = $user_profile->get_user_profile();
-	$f_name = $user['f_name'] ;
+	if (isset($_SESSION['user'])) return  header('Location:../login.php');
+	// require '../backends/classes/DBConnect.php';
+	// require './backends/classes/UserProfile.php';
+	// require '../backends/classes/HospitalSetup.php';
+	// require '../backends/classes/FormValidator.php';
+	// $id = $_SESSION['user'];
+	// $user_profile = new UserProfile($id);
+	// $user = $user_profile->get_user_profile();
+	// $f_name = $user['f_name'] ;
 	
 ?>
