7a5edc64d2d89be687ec576c669a68ad6079d4f9
davien21 <chidiebereekennia@gmail.com>
2020-08-22 21:23:46 +0100

    testing __DIR__ bug -6

--- user/home.php
+++ user/home.php
@@ -2,7 +2,7 @@
 		require './backends/home-b.php';
 		// echo __DIR__."<br>"; 
 		// foreach ($_SERVER as $key => $value) {
-		
+
 		// 	echo "{$key} : {$value}<br>";
 		// }
 	?>
@@ -11,33 +11,34 @@
 		$request = $_SERVER['REQUEST_URI'];
 		$request = substr($request, strrpos($request,'/'));
 		if ($request === '/') {
-			
+
 			require 'user-menu.php'; user_menu('home','Home');
         	require __DIR__.'/views/first-login.php';
 		}
 		else if ($request === '/hospital-setup') {
-			
+
 			require 'user-menu.php'; user_menu('home','Hospital Setup');
 	        require __DIR__.'/views/hospital-setup.php';
 		}
 		else if ($request === '/select-hospital-tools') {
-			
+
 			require 'user-menu.php'; user_menu('home','Select Hospital Tools');
 	        require __DIR__.'/views/select-hospital-tools.php';
 			
 		}
 		else if ($request === '/join-hospital') {
-			
+
 			require 'user-menu.php'; user_menu('home','Join Hospital');
 	        require __DIR__.'/views/join-hospital.php';
 			
 		}
 		else if ($request === '/logout') {
-			
+
 	        header('Location:../backends/logout.php');
 		}
 		else {
-	        header('Location:../404.php');
+			echo $request;
+	        // header('Location:../404.php');
 
 		}
 	?>
