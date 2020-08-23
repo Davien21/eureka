c5ede2c1f5655fff81b56c66b1391b66a9f58d2f
davien21 <chidiebereekennia@gmail.com>
2020-08-23 23:45:03 +0100

    omo

--- user/index.php
+++ user/index.php
@@ -9,31 +9,31 @@
 	
 	<?php 
 		$request = $_SERVER['REQUEST_URI'];
-		$request = substr($request, strrpos($request,'/'));
-		if ($request === '/') {
-			// echo $request."<br>";
+			echo $request."<br>";
+		// $request = substr($request, strrpos($request,'/'));
+		if ($request === '/user/') {
 			// echo __DIR__;
 			require 'user-menu.php'; user_menu('home','Home');
         	require __DIR__.'/views/first-login.php';
 		}
-		else if ($request === '/hospital-setup') {
+		else if ($request === '/user/hospital-setup') {
 
 			require 'user-menu.php'; user_menu('home','Hospital Setup');
 	        require __DIR__.'/views/hospital-setup.php';
 		}
-		else if ($request === '/select-hospital-tools') {
-
-			require 'user-menu.php'; user_menu('home','Select Hospital Tools');
-	        require __DIR__.'/views/select-hospital-tools.php';
-			
-		}
-		else if ($request === '/join-hospital') {
+		else if ($request === '//user/join-hospital') {
 
 			require 'user-menu.php'; user_menu('home','Join Hospital');
 	        require __DIR__.'/views/join-hospital.php';
 			
 		}
-		else if ($request === '/logout') {
+		else if ($request === '//user/select-hospital-tools') {
+
+			require 'user-menu.php'; user_menu('home','Select Hospital Tools');
+	        require __DIR__.'/views/select-hospital-tools.php';
+			
+		}
+		else if ($request === '//user/logout') {
 
 	        header('Location:../backends/logout.php');
 		}
