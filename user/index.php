4dc007e8d274795ac80626ef97760227ea53bd02
davien21 <chidiebereekennia@gmail.com>
2020-08-24 00:01:42 +0100

    omo

--- user/index.php
+++ user/index.php
@@ -6,13 +6,12 @@
 		// 	echo "{$key} : {$value}<br>";
 		// }
 	?>
-	
+
 	<?php 
 		$request = $_SERVER['REQUEST_URI'];
 		$request = substr($request, strrpos($request,'/'));
 		if ($request === '/') {
-			// echo $request."<br>";
-			// echo __DIR__;
+
 			require 'user-menu.php'; user_menu('home','Home');
         	require __DIR__.'/views/first-login.php';
 		}
@@ -21,16 +20,16 @@
 			require 'user-menu.php'; user_menu('home','Hospital Setup');
 	        require __DIR__.'/views/hospital-setup.php';
 		}
-		else if ($request === '/join-hospital') {
-
-			require 'user-menu.php'; user_menu('home','Join Hospital');
-	        require __DIR__.'/views/join-hospital.php';
-			
-		}
 		else if ($request === '/select-hospital-tools') {
 
 			require 'user-menu.php'; user_menu('home','Select Hospital Tools');
 	        require __DIR__.'/views/select-hospital-tools.php';
+			
+		}
+		else if ($request === '/join-hospital') {
+
+			require 'user-menu.php'; user_menu('home','Join Hospital');
+	        require __DIR__.'/views/join-hospital.php';
 			
 		}
 		else if ($request === '/logout') {
@@ -40,7 +39,7 @@
 		else {
 			echo $request;
 			echo __DIR__;
-	        header('Location:../404.php');
+	        // header('Location:../404.php');
 
 		}
 	?>
